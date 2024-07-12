00eb17540a81a9bb51852e67cfafb5307435340f
Bonkersy <86378317+Bonkersy@users.noreply.github.com>
2024-07-12 12:58:05 +0300

    Update script.js

--- hamster_exchange/assets/js/script.js
+++ hamster_exchange/assets/js/script.js
@@ -16,16 +16,27 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
+
+  if(window.Telegram)
+  {
+    const tele = window.Telegram.WebApp;
+    tele.ready();
+    tele.expand();
+  }
+
   // курс валюты (1 рубль = 0.016 евро)
   var exchangeRate = 0.00010023;
 
   // установка значения по умолчанию в #input-hams
-  $('#input-hams').val(1000000);
+  $('#input-hams').val(100000);
 
   // функция конвертации валюты
   function convertCurrency() {
     var amount = parseFloat($('#input-hams').val()); // значение из input в рублях
     var convertedAmount = amount * exchangeRate; // конвертируем в евро
+
+    if(isNaN(convertedAmount)) convertedAmount = 0;
+
     $('#input-usdt').val(convertedAmount.toFixed(2)); // выводим результат в input в евро
   }
 
